5cd75be4356ea48e41bd1f5d9fcd5234c9f0af9a
VINX <vinxgithubacc@gmail.com>
2025-11-09 10:14:03 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -151,17 +151,17 @@ router.get('/', async (req, res) => {
                             
                             // Send video thumbnail with caption
                             await sock.sendMessage(userJid, {
-                                image: { url: 'https://img.youtube.com/vi/-oz_u1iMgf8/maxresdefault.jpg' },
-                                caption: `🎬 *KnightBot MD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/-oz_u1iMgf8`
+                                image: { url: 'https://img.youtube.com/vi/yourimglink' },
+                                caption: `🎬 *Ft.Bot MD V0.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/yourlink`
                             });
                             console.log("🎬 Video guide sent successfully");
                             
                             // Send warning message
                             await sock.sendMessage(userJid, {
                                 text: `⚠️Do not share this file with anybody⚠️\n 
-┌┤✑  Thanks for using Knight Bot
+┌┤✑  Thanks for using Ft.Bot
 │└────────────┈ ⳹        
-│©2024 Mr Unique Hacker 
+│©2025 VINX.JS 
 └─────────────────┈ ⳹\n\n`
                             });
                         } else {
